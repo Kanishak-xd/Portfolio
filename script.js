31574a6f081b1494d2d76079ea8f556eb3670610
Kanishak <151437546+Kanishak-xd@users.noreply.github.com>
2024-06-03 02:08:35 +0530

    Update script.js
    Responsive Design

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 // Get the button:
 let mybutton = document.getElementById("toTheTopButton");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls, check if they have reached the bottom or if they have scrolled up to show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if ((document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) && 
+      (window.innerHeight + window.scrollY) < document.body.offsetHeight) {
+    // Show button = yes if scroll (20px atleast) = yes but bottom = no
     mybutton.style.display = "block";
   } else {
+    // If user is at the bottom or hasn't scrolled down 20px, hide the button
     mybutton.style.display = "none";
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+// To the Top Button
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, Internet Explorer and Opera
 }
-
